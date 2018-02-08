03312f0f00cb565cadfc96d1c698169ee2876ed7
Narongsak <narongsak.chobsri@gmail.com>
2018-02-08 18:00:01 +0700

    Add javascript module

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,29 +11,10 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
-//= require_tree .
 //= require jquery
 //= require bootstrap
-$(document).ready(function(){       
-	// $("")
-	$("#search-input").hide();
-	$("#search-button").on("click",function() {
-		$("#login").fadeOut();
-		$("#cart").fadeOut();
-		$('#search-button').fadeOut(function () {
-			$('#search-input').animate({
-				width: "toggle"
-			});
-	});
-	});
-	$("#close-search").on("click", function() {
-		$('#search-input').animate({
-			width: "toggle"
-		}, function() {
-			$("#login").fadeIn();
-			$("#cart").fadeIn();
-			$('#search-button').fadeIn();
-		});
-	});
-});	
+
+//= require ./landing
+//= require ./app
+
+//= require turbolinks
