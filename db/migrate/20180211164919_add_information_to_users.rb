30916cc7f2089e036f745370480e1872a90a1a6f
Narongsak <narongsak.chobsri@gmail.com>
2018-02-11 23:58:34 +0700

    Add information column to users

---
+++ db/migrate/20180211164919_add_information_to_users.rb
@@ -0,0 +1,12 @@
+class AddInformationToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :text
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :country, :string
+    add_column :users, :zip_code, :string
+    add_column :users, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211162723) do
+ActiveRecord::Schema.define(version: 20180211164919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,14 @@ ActiveRecord::Schema.define(version: 20180211162723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.string "first_name"
+    t.string "last_name"
+    t.text "address"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.string "zip_code"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
