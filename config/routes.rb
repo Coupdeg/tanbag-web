8d7dfcad8bcc02d58b14a3af2b807e4106b10cdb
Narongsak <narongsak.chobsri@gmail.com>
2018-02-17 22:32:32 +0700

    Add routes product

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   scope 'api/v1', module: 'api/v1', as: 'api_v1' do
     resources :products
   end
+
+  resources :products, only: [:index, :show, :edit, :new]
+
   devise_for :users
   root to: 'landing#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
